7812c0a60953a4751cdb3d2e4a8125f361898002
MojitoBar <wnehdtjr5@naver.com>
2022-04-19 13:34:27 +0900

    first init

---
+++ Scouter/ViewController/SearchViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HomeViewController.swift
+//  Scouter
+//
+//  Created by judongseok on 2022/04/19.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .orange
+    }
+}
---
+++ Scouter/ViewController/TabBarViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Scouter
+//
+//  Created by judongseok on 2022/04/19.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .purple
+    }
+}
+
