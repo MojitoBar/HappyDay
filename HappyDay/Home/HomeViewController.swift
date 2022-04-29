f5a0b1841e17c80585be510df31ba8cf129a4f42
MojitoBar <wnehdtjr5@gmail.com>
2022-04-30 00:53:49 +0900

    Feat: Apply RxSwift

--- HappyDay/Home/HomeViewController.swift
+++ HappyDay/Home/HomeViewController.swift
@@ -7,16 +7,40 @@
 
 import UIKit
 import SnapKit
-import Charts
+import RxSwift
+import RxCocoa
 
 class HomeViewController: UIViewController {
+    let viewModel = HomeViewModel()
+    var disposeBag = DisposeBag()
+    
     // MARK: - view life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        setTable()
+        
+        viewModel.personObservable
+            .observe(on: MainScheduler.asyncInstance)
+            .bind(to: tableView.rx.items(cellIdentifier: PersonCell.cellId, cellType: PersonCell.self)) { index, item, cell in
+                cell.name.text = item.name
+                cell.phoneNumber.text = item.phoneNumber
+            }
+            .disposed(by: disposeBag)
     }
     
     // MARK: - ui setting
-    func setUI() {
-        
+    let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.rowHeight = 100
+        return tableView
+    }()
+    
+    // MARK: - table setting
+    func setTable() {
+        self.view.addSubview(tableView)
+        tableView.register(PersonCell.self, forCellReuseIdentifier: PersonCell.cellId)
+        tableView.snp.makeConstraints {
+            $0.leading.trailing.top.bottom.equalTo(self.view)
+        }
     }
 }
