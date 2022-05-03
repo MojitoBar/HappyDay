c175098cc1d48f96d444a2cc4054ced3d3404e98
MojitoBar <wnehdtjr5@gmail.com>
2022-05-03 23:46:19 +0900

    Change: Consonant 오류 수정

--- HappyDay/Extension/String+Extension.swift
+++ HappyDay/Extension/String+Extension.swift
@@ -9,13 +9,18 @@ import Foundation
 
 extension String {
     func getFirstChar() -> String{
+        guard self.first != nil else { return "" }
         let hangul = ["ㄱ","ㄲ","ㄴ","ㄷ","ㄸ","ㄹ","ㅁ","ㅂ","ㅃ","ㅅ","ㅆ","ㅇ","ㅈ","ㅉ","ㅊ","ㅋ","ㅌ","ㅍ","ㅎ"]
-        let pattern = "[가-힣]"
-        if (try? NSRegularExpression(pattern: pattern, options: .caseInsensitive)) != nil {
-            return self.first!.description
+        let pattern = "^[가-힣]*"
+        
+        if (try? NSRegularExpression(pattern: pattern, options: [])) != nil {
+            let octal = self.unicodeScalars[self.unicodeScalars.startIndex].value
+            if octal < 44032 {
+                return self.first!.description
+            }
+            let index = (octal - 0xac00) / 28 / 21
+            return hangul[Int(index)]
         }
-        let octal = self.unicodeScalars[self.unicodeScalars.startIndex].value
-        let index = (octal - 0xac00) / 28 / 21
-        return hangul[Int(index)]
+        return self.first!.description
     }
 }
