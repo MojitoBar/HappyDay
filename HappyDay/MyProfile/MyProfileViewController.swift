779bd56bd48e445aa7ab0c5cb82fee729eb66941
MojitoBar <wnehdtjr5@gmail.com>
2022-05-12 00:22:58 +0900

    Feat: MyProfileVC

--- HappyDay/MyProfile/MyProfileViewController.swift
+++ HappyDay/MyProfile/MyProfileViewController.swift
@@ -5,4 +5,28 @@
 //  Created by judongseok on 2022/05/12.
 //
 
-import Foundation
+import UIKit
+import RxSwift
+
+class MyProfileViewController: UIViewController, CustomViewController {
+    // MARK: - view life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - ui setting
+    let titleLabel: UILabel = {
+        let title = UILabel()
+        return title
+    }()
+    
+    // MARK: - layout setting
+    func setLayout() {
+        self.view.addSubview(titleLabel)
+    }
+    
+    // MARK: - Rx Setting
+    func setRx() {
+        
+    }
+}
