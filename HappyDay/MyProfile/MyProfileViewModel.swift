330b1b155a05688ff55ae7ad700b509a1ee45e9f
MojitoBar <wnehdtjr5@gmail.com>
2022-05-18 01:34:07 +0900

    Change: 뷰 요소들 rx로 모두 바인딩

--- HappyDay/MyProfile/MyProfileViewModel.swift
+++ HappyDay/MyProfile/MyProfileViewModel.swift
@@ -12,27 +12,66 @@ import RxRelay
 class MyProfileViewModel {
     
     // OUTPUT
-    let nameObservable: Observable<String>
-    let birthdayObservable: Observable<String>
-    let profileObservable: Observable<UIImage>
-//    let 
+    var nameObservable: Observable<String>!
+    var birthdayObservable: Observable<String>!
+    var profileObservable: Observable<UIImage>!
+    
+    var wishFirstImg: Observable<UIImage>!
+    var wishFirstContent: Observable<String>!
+    var wishFirstPrice: Observable<String>!
+    var wishFirstName: Observable<String>!
+    var wishSecondImg: Observable<UIImage>!
+    var wishSecondContent: Observable<String>!
+    var wishSecondPrice: Observable<String>!
+    var wishSecondName: Observable<String>!
     
     let disposeBag = DisposeBag()
     
     init() {
-        let fetchUser = BehaviorRelay<User>(value: User())
+        fetchUser()
+        fetchWishList()
+    }
+    
+    func fetchUser() {
+        let user = BehaviorRelay<User>(value: User())
         
         FetchUser.fetchUsersRx()
-            .bind(to: fetchUser)
+            .bind(to: user)
             .disposed(by: disposeBag)
         
-        nameObservable = fetchUser
+        nameObservable = user
             .map { $0.name }
         
-        birthdayObservable = fetchUser
+        birthdayObservable = user
             .map { $0.birthday }
         
-        profileObservable = fetchUser
+        profileObservable = user
             .map { ($0.profile ?? UIImage.init(systemName: "heart"))! }
+    }
+    
+    func fetchWishList(){
+        let wishList = BehaviorRelay<[WishItem]>(value: [])
+        
+        FetchWishList.fetchWishListRx()
+            .bind(to: wishList)
+            .disposed(by: disposeBag)
+        
+        wishFirstName = wishList
+            .map { $0[0].name }
+        wishFirstImg = wishList
+            .map { ($0[0].image ?? UIImage.init(systemName: "person"))! }
+        wishFirstPrice = wishList
+            .map { $0[0].price }
+        wishFirstContent = wishList
+            .map { $0[0].content }
+        
+        wishSecondName = wishList
+            .map { $0[1].name }
+        wishSecondImg = wishList
+            .map { ($0[1].image ?? UIImage.init(systemName: "person"))! }
+        wishSecondPrice = wishList
+            .map { $0[1].price }
+        wishSecondContent = wishList
+            .map { $0[1].content }
     }
 }
---
+++ HappyDay/MyProfile/View/MyProfileView.swift
@@ -0,0 +1,80 @@
+//
+//  MyProfileView.swift
+//  HappyDay
+//
+//  Created by judongseok on 2022/05/18.
+//
+
+import UIKit
+
+extension MyProfileViewController {
+    // MARK: - layout setting
+    func setLayout() {
+        self.view.backgroundColor = .white
+        self.navigationController?.isNavigationBarHidden = true
+        
+        self.view.addSubview(titleLabel)
+        self.view.addSubview(profile)
+        self.view.addSubview(name)
+        self.view.addSubview(birthdayLabel)
+        self.view.addSubview(wishLabel)
+        self.view.addSubview(givenLabel)
+        self.view.addSubview(wishCardContainView)
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(self.view.safeAreaLayoutGuide).offset(20)
+            $0.leading.equalTo(20)
+        }
+        profile.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(20)
+            $0.leading.equalTo(20)
+            $0.width.height.equalTo(80)
+        }
+        name.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(30)
+            $0.leading.equalTo(profile.snp.trailing).offset(20)
+        }
+        birthdayLabel.snp.makeConstraints {
+            $0.top.equalTo(name.snp.bottom).offset(10)
+            $0.leading.equalTo(profile.snp.trailing).offset(20)
+        }
+        wishLabel.snp.makeConstraints {
+            $0.top.equalTo(profile.snp.bottom).offset(30)
+            $0.leading.equalTo(20)
+        }
+        wishCardContainView.snp.makeConstraints {
+            $0.top.equalTo(wishLabel.snp.bottom).offset(10)
+            $0.leading.equalTo(0)
+            $0.trailing.equalTo(0)
+            $0.height.equalTo(dummyWishItems.count * 100)
+        }
+        for i in 0..<wishCards.count {
+            wishCardContainView.addSubview(wishCards[i])
+            wishCards[i].snp.makeConstraints {
+                $0.top.equalTo(i * 100)
+                $0.leading.equalTo(0)
+                $0.trailing.equalTo(0)
+            }
+        }
+        givenLabel.snp.makeConstraints {
+            $0.top.equalTo(wishCardContainView.snp.bottom)
+            $0.leading.equalTo(20)
+        }
+    }
+    
+    // MARK: - ScrollView Setting
+    func setScrollView() {
+        self.view.addSubview(givenGiftScrollView)
+        
+        givenGiftScrollView.frame = CGRect(x: 0, y: 0, width: self.view.bounds.width, height: 100)
+        givenGiftScrollView.addSubview(GivenItemView(img: UIImage.init(systemName: "star")!))
+        givenGiftScrollView.contentSize = CGSize(width: 1000, height: 100)
+        
+        givenGiftScrollView.snp.makeConstraints {
+            $0.top.equalTo(givenLabel.snp.bottom).offset(10)
+            $0.leading.equalTo(0)
+            $0.height.equalTo(100)
+            $0.width.equalTo(self.view.snp.width)
+        }
+    }
+}
